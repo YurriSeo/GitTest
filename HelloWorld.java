37a5509269152a6ef615bbe641588ff0176a7e75
Yuri Seo <syr5800@lotte.net>
2019-03-11 09:28:08 +0900

    First Git Test

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Ssangah Test 333!!");
+		System.out.println("Yuri Test 111!");
 	}
 }
